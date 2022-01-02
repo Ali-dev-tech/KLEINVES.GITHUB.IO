587606a9706b0cdba3a42b9ee1bbf4f2946aeaac
Ali-dev-tech <82727465+Ali-dev-tech@users.noreply.github.com>
2022-01-02 17:28:33 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var burger = document.querySelector('.burger')
+var menu = document.querySelector('.menu-list')
+
+burger.addEventListener('click',function(){
+    menu.classList.toggle('display')
+})
+
